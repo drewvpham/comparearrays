d1b476b211cf426df9e0593c0fc1429dc5bfe70f
Drew Pham <drewvpham@gmail.com>
2017-05-09 08:39:36 -0500

    completed

---
+++ ca.py
@@ -0,0 +1,7 @@
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','cream']
+
+if list_one == list_two:
+    print "the lists are the same"
+else:
+    print "these are not identical"
